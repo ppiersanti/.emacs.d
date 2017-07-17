3989ece7f4f1f25cdb155ddf8aae18f48387597d
gon <paolo@micso.it>
2017-07-17 13:35:19 +0200

    several changes

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-
+(set 'ad-redefinition-action 'accept)
 (global-font-lock-mode 1)
 (setq font-lock-maximum-decoration t)
 (setq scroll-step 1)
@@ -29,8 +29,8 @@
       kept-new-versions 10               ; newest versions to keep when a new numbered backup is made (default: 2)
       auto-save-default t               ; auto-save every buffer that visits a file
       auto-save-timeout 30              ; number of seconds idle time before auto-save (default: 30)
-      auto-save-interval 300            ; number of keystrokes between auto-saves (default: 300)
-      )
+      auto-save-interval 300)            ; number of keystrokes between auto-saves (default: 300)
+      
 
 
 (setq compilation-ask-about-save nil)
@@ -49,14 +49,14 @@
 ;; map org file to org-mode
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
-					;(setq visible-bell t)
+          ;(setq visible-bell t)
 (setq ring-bell-function
       (lambda ()
-	(unless (memq this-command
-		      '(isearch-abort abort-recursive-edit
-				      exit-minibuffer keyboard-quit))
-	  (invert-face 'mode-line)
-	  (run-with-timer 0.1 nil 'invert-face 'mode-line))))
+  (unless (memq this-command)
+          '(isearch-abort abort-recursive-edit
+              exit-minibuffer keyboard-quit)
+    (invert-face 'mode-line)
+    (run-with-timer 0.1 nil 'invert-face 'mode-line))))
 
 (display-time)
 
@@ -85,10 +85,11 @@
  '(cider-repl-use-pretty-printing t)
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (deeper-blue)))
+ '(ido-everywhere t)
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (undo-tree smex projectile paxedit markdown-mode ido-ubiquitous groovy-mode git-gutter company clojure-mode-extra-font-locking clj-refactor cider-eval-sexp-fu auto-highlight-symbol aggressive-indent adoc-mode))))
+    (parinfer kibit-helper cloc ac-cider sr-speedbar undo-tree smex projectile paxedit markdown-mode ido-ubiquitous groovy-mode git-gutter company clojure-mode-extra-font-locking clj-refactor cider-eval-sexp-fu auto-highlight-symbol aggressive-indent adoc-mode))))
 
 
 
@@ -96,7 +97,7 @@
 
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-	;;        ("melpa-stable" . "https://stable.melpa.org/packages/")
+  ;;        ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize) ;; You might already have this line
 
@@ -142,12 +143,12 @@
         (while (> count 0)
           (newline)         ;; because there is no newline in 'line'
           (insert line)
-          (setq count (1- count)))
-        )
+          (setq count (1- count))))
+        
 
       ;; create the undo information
-      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
-    ) ; end-of-let
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list))))
+     ; end-of-let
 
   ;; put the point in the lowest line and return
   (next-line arg))
@@ -160,30 +161,36 @@
 ;; cider cljs nRepl conf 
 (require 'cider)
 (setq cider-cljs-lein-repl
-      "(do (require 'figwheel-sidecar.repl-api)
-           (figwheel-sidecar.repl-api/start-figwheel!)
-           (figwheel-sidecar.repl-api/cljs-repl))")
+      "(do (user/run)
+           (user/browser-repl))")
+      ;; "(do (require 'figwheel-sidecar.repl-api)
+      ;;      (figwheel-sidecar.repl-api/start-figwheel!)
+      ;;      (figwheel-sidecar.repl-api/cljs-repl))"
+      
 
 ;;
-(require 'paxedit)
+;;(require 'paxedit)
 
 (setq ido-enable-flex-matching t)
 (ido-mode 1)
 (ido-everywhere t)
 
-(require 'ido-ubiquitous)
+(require 'ido-completing-read+)
 (ido-ubiquitous-mode 1)
 
 (global-company-mode)
 (global-git-gutter-mode +1)
 
 
-(add-hook 'clojure-mode-hook #'paredit-mode)
-(add-hook 'clojure-mode-hook #'paxedit-mode)
-(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+;; (add-hook 'clojure-mode-hook #'paredit-mode)
+;; (add-hook 'clojure-mode-hook #'paxedit-mode)
+;; (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 (add-hook 'clojure-mode-hook #'subword-mode)
-(add-hook 'cider-repl-mode-hook #'paredit-mode)
-(add-hook 'cider-repl-mode-hook #'paxedit-mode)
+(add-hook 'cider-mode-hook #'eldoc-mode)
+
+
+;; (add-hook 'cider-repl-mode-hook #'paredit-mode)
+;; (add-hook 'cider-repl-mode-hook #'paxedit-mode)
 (add-hook 'cider-repl-mode-hook #'subword-mode)
 (add-hook 'cider-repl-mode-hook #'eldoc-mode)
 ;;(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)  ;; does not work
@@ -192,7 +199,7 @@
 (setq cider-font-lock-dynamically '(macro core function var))
 (setq clojure-align-forms-automatically t)
 (setq cider-prompt-save-file-on-load 'always-save)
-(add-hook 'cider-mode-hook #'eldoc-mode)
+
 (setq cider-test-show-report-on-success t)
 (setq cider-repl-history-file "~/.emacs.d/cider-nrepl.history")
 (setq cider-prompt-for-symbol nil)
@@ -216,6 +223,7 @@
 (defun my-clojure-mode-hook ()
   (clj-refactor-mode 1)
   (yas-minor-mode 1) ; for adding require/use/import statements
+  (yas-reload-all)
   ;; This choice of keybinding leaves cider-macroexpand-1 unbound
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
@@ -227,10 +235,10 @@
 (global-set-key (quote [F5]) 'cider-run)
 
 (add-hook 'cider-mode-hook
-	  '(lambda () (add-hook 'after-save-hook
-				'(lambda ()
-				   (if (and (boundp 'cider-mode) cider-mode)
-				       (cider-refresh))))))
+    '(lambda () (add-hook 'after-save-hook)
+        '(lambda ()
+           (if (and (boundp 'cider-mode) cider-mode)
+               (cider-refresh)))))
 
 (defun cider-namespace-refresh ()
   (interactive)
@@ -249,14 +257,52 @@
 
 
 
-(custom-set-faces
+(custom-set-faces)
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ 
 
 
 ;;; Smex
 (autoload 'smex "smex")
 (global-set-key (kbd "<M-x S>") 'smex)
+
+
+
+;; (require 'ac-cider)
+;; (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+;; (add-hook 'cider-mode-hook 'ac-cider-setup)
+;; (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+;; (eval-after-load "auto-complete"
+;;     '(add-to-list 'ac-modes 'cider-mode))
+
+
+(require 'sr-speedbar)
+(require 'use-package)
+
+(use-package parinfer
+  :ensure t
+  :bind
+  (("C-," . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+	     pretty-parens  ; different paren styles for different modes.
+	     evil           ; If you use Evil.
+	     lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+	     paredit        ; Introduce some paredit commands.
+	     smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+	     smart-yank))   ; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'scheme-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)
+    (add-hook 'cider-repl-mode-hook #'parinfer-mode)))
+
+(setq parinfer-auto-switch-indent-mode t)
+(setq parinfer-auto-switch-indent-mode-when-closing t)  ;; default
+(setq parinfer-extensions '(defaults pretty-parens smart-yank))
