ada3884d318c5a2909c3a584553b0b6327731f6d
gon <paolo@micso.it>
2017-07-19 15:48:32 +0200

    recover paredit setup

--- init.el
+++ init.el
@@ -182,15 +182,15 @@
 (global-git-gutter-mode +1)
 
 
-;; (add-hook 'clojure-mode-hook #'paredit-mode)
-;; (add-hook 'clojure-mode-hook #'paxedit-mode)
-;; (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'paxedit-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 (add-hook 'clojure-mode-hook #'subword-mode)
 (add-hook 'cider-mode-hook #'eldoc-mode)
 
 
-;; (add-hook 'cider-repl-mode-hook #'paredit-mode)
-;; (add-hook 'cider-repl-mode-hook #'paxedit-mode)
+(add-hook 'cider-repl-mode-hook #'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'paxedit-mode)
 (add-hook 'cider-repl-mode-hook #'subword-mode)
 (add-hook 'cider-repl-mode-hook #'eldoc-mode)
 ;;(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)  ;; does not work
@@ -280,29 +280,3 @@
 
 
 (require 'sr-speedbar)
-(require 'use-package)
-
-(use-package parinfer
-  :ensure t
-  :bind
-  (("C-," . parinfer-toggle-mode))
-  :init
-  (progn
-    (setq parinfer-extensions
-          '(defaults       ; should be included.
-	     pretty-parens  ; different paren styles for different modes.
-	     evil           ; If you use Evil.
-	     lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-	     paredit        ; Introduce some paredit commands.
-	     smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-	     smart-yank))   ; Yank behavior depend on mode.
-    (add-hook 'clojure-mode-hook #'parinfer-mode)
-    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'scheme-mode-hook #'parinfer-mode)
-    (add-hook 'lisp-mode-hook #'parinfer-mode)))
-
-(setq parinfer-auto-switch-indent-mode t)
-(setq parinfer-auto-switch-indent-mode-when-closing t)  ;; default
-(setq parinfer-extensions '(defaults pretty-parens smart-yank))
-(setq parinfer-lighters '("Parinfer:Indent" . "Parinfer:Paren"))
