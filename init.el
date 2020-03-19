675650da935aa40dbf03520a2f235440a16e1a8a
Paolo Piersanti (gon) <paolo@micso.it>
2020-03-19 10:46:20 +0100

    change ccmment face

--- init.el
+++ init.el
@@ -394,6 +394,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "color-142"))))
  '(region ((t (:background "gold" :distant-foreground "gtk_selection_fg_color")))))
 
 
